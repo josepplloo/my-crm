49e810909a826bc2cdeb8f0ab3651a578fdc3143
Jose Miguel <josepplloo@gmail.com>
2021-10-09 19:19:01 -0500

    Users creation (#2)
    * input resolver example
    
    * mongoose model, typeDef, resolvers and hashing password for Users

--- db/schema.js
+++ db/schema.js
@@ -17,6 +17,26 @@ const typeDefs = gql`
     getCourses(input: InputCourse!): [Course]
     getTechs: [Tech]
   }
+
+  type User {
+    id: ID
+    name: String
+    surname: String
+    email: String
+    created: String
+
+  }
+
+  input InputUser {
+    name: String!
+    surname: String!
+    email: String!
+    password: String!
+  }
+  
+  type Mutation {
+    newUser(input: InputUser): User
+  }
 `;
 
 module.exports = typeDefs;
